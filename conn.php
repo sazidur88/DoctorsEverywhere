3676cdcee2ea612005fb140206721ab865a8e6b9
Sazidur Rahman Sadi <sazidurrahmansadi@gmail.com>
2019-12-15 21:37:17 +0600

    deleted

--- conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbServerName = "localhost";
-$dbUserName = "root";
-$dbPass = "";
-$dbName = "doctorseverywhere";
-
-$conn = mysqli_connect($dbServerName, $dbUserName, $dbPass, $dbName);
-?>
